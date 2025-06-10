c2170bf4e570792915718be0378fff7c52faa78f
XabierF <sceptileen@gmail.com>
2025-06-10 13:43:09 +0200

    actualización GamemManager

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -8,9 +8,12 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField] GameObject npcPrefab;
-    [SerializeField] List<Transform> spawnPoints;
-    [SerializeField] int maxNPCs = 20;
-    [SerializeField] List<GameObject> npcs = new List<GameObject>();
+    [SerializeField] List<Transform> spawnPoints; //Puntos de instancia
+    [SerializeField] int maxAforo = 20;
+    int currentAforo;
+
+    [SerializeField] List<GameObject> normalConsumers = new List<GameObject>();
+    [SerializeField] List<GameObject> problematicConsumers = new List<GameObject>();
 
     void Awake()
     {
@@ -24,11 +27,29 @@ public class GameManager : MonoBehaviour
         {
             if (obj.CompareTag("Consumer"))
             {
-                npcs.Add(obj);
+                normalConsumers.Add(obj);
             }
         }
         allGameObjects = null;
-        InvokeRepeating("SpawnNPC", 0f, 5f); //DEBUG: Spawns an NPC every 5 seconds 
+
+    }
+
+    void Update()
+    {
+        if (normalConsumers.Count < maxAforo)
+        {
+            SpawnNPC();
+        }
+    }
+
+    private void RefreshAforo()
+    {
+        currentAforo = normalConsumers.Count + problematicConsumers.Count;
+        Debug.Log("Current Aforo: " + currentAforo);
+        if (currentAforo < maxAforo * 0.2f)
+        {
+            GameOver();
+        }
     }
 
 
@@ -39,19 +60,30 @@ public class GameManager : MonoBehaviour
 
     void HandleDespawnRequest(NPCBehavior npc)
     {
-        npcs.Remove(npc.gameObject);
+        if (normalConsumers.Contains(npc.gameObject))
+        {
+            normalConsumers.Remove(npc.gameObject);
+        }
+        else if (problematicConsumers.Contains(npc.gameObject))
+        {
+            problematicConsumers.Remove(npc.gameObject);
+        }
         Destroy(npc.gameObject);
-        
     }
 
     public void SpawnNPC()
     {
-        if (npcs.Count < maxNPCs)
+        if (currentAforo < maxAforo)
         {
             Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)]; //spawnean el npc en el un punto de spawn (puertas)
             GameObject newNPC = Instantiate(npcPrefab, spawnPoint.position, Quaternion.identity);
-            npcs.Add(newNPC);
+            normalConsumers.Add(newNPC);
         }
     }
-    
+
+    //game over si el aforo actual es menos al 20% del maximo
+    void GameOver()
+    {
+        // Time.timeScale = 0f;
+    }
 }
